117240e5511c0c6f53ea6822956bf214b8525736
Bianca Henderson <beeankha@gmail.com>
2022-05-07 10:57:01 -0400

    More changes to the main file, cleaning up output options

--- cli_tarot/__main__.py
+++ cli_tarot/__main__.py
@@ -65,7 +65,7 @@ def main():
         (passed as an integer after the '-fd'
         flag; defaults to a single-card pull)
                        """,
-                       required=False,
+                       # required=False,
                        )
 
     group.add_argument("-s", "--seen_heard_held",
@@ -205,9 +205,11 @@ def main():
 
     # TODO: Choices Spread
 
+    # When someone tries to look up a card but forgets to specify card index
     elif args.card == 1 and args.meaning is None:
         print("You need to specify a card ID! (choose between 0 and 77)")
 
+    # Looking up a card's meaning can only output to the terminal vs outputs to a file
     elif args.card == 1 and args.meaning < 78:
         meaning = list_index.index(args.meaning)
         card = str(card_directory.card_dict[args.meaning])
@@ -253,15 +255,14 @@ def main():
             print(f"\n{ascii_art.card_art[index]}\n")
             print(f"\n{card_meanings.meanings[index]}\n")
             if args.output is True:
-                with open("Single_Card_Art_and_Interpretation_" + timestr + '.txt', 'a') as output_file:
-                    output_file.write(f"{ascii_art.card_art[index]}\n" + "---\n" + f"{card_meanings.meanings[index]}\n" + "---\n")
+                output.output_meaning("Single_Card_Art_and_Interpretation_", index)
 
         if args.interpretation is False and args.art is False:
             if args.output is True:
                 output.output_reading_only("Single_Card_Reading_", card_directory.card_dict[index])
 
-    # For "Free Draw" readings, arbitrary number of cards
-    elif args.free > 1:
+    # For "Free Draw" readings, which involves any number of cards between 1 and 78
+    elif args.free in range(1, 79):
         cards = random.sample(reading, k=args.free)
         print(f"\n✨ You have pulled the following {args.free} cards: ✨\n")
         time.sleep(1.5)
@@ -275,8 +276,7 @@ def main():
                 time.sleep(2)
                 print(f"\n{card_meanings.meanings[index]}\n")
                 if args.output is True:
-                    with open("Free_Draw_Interpreted_" + timestr + '.txt', 'a') as output_file:
-                        output_file.write(f"{card_meanings.meanings[index]}\n" + "---\n")
+                    output.output_meaning(f"{args.free}_Card_Draw_Interpreted_", index)
 
             if args.interpretation is True and args.art is True:
                 print("...interpreting and generating card art...")
@@ -284,14 +284,21 @@ def main():
                 print(f"\n{ascii_art.card_art[index]}\n")
                 print(f"\n{card_meanings.meanings[index]}\n")
                 if args.output is True:
-                    with open("Free_Draw_Art_and_Interpretation_" + timestr + '.txt', 'a') as output_file:
-                        output_file.write(f"{ascii_art.card_art[index]}\n" + "---\n")
-                        output_file.write(f"{card_meanings.meanings[index]}\n" + "---\n")
+                    output.output_meaning_and_art(f"{args.free}_Card_Draw_Art_and_Interpretation_", index)
 
             if args.interpretation is False and args.art is True:
                 print("...generating card art...")
                 time.sleep(2)
                 print(f"\n{ascii_art.card_art[index]}\n")
                 if args.output is True:
-                    with open("Free_Draw_Art_Only_" + timestr + '.txt', 'a') as output_file:
-                        output_file.write(f"{ascii_art.card_art[index]}\n" + "---\n")
+                    output.output_art(f"{args.free}_Card_Draw_Art_Only_", index)
+
+            if args.interpretation is False and args.art is False and args.output is True:
+                reading = card_directory.card_dict[index]
+                output.output_reading_only(f"{args.free}_Card_Reading_", reading)
+
+    elif args.free not in range(1, 79):
+        print("""
+            You can't select a number of cards smaller than zero or
+            larger than the deck! (min: 1, max: 78)
+        """)
